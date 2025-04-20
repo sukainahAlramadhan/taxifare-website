44eadde9cb46e67302c77f39db5afcb8b0b4b6e3
Sukainah Alramadhan <sukainahalramadhan@gmail.com>
2025-04-20 14:01:45 +0300

    Finish the first Challenghe

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ import requests
 # TaxiFareModel front
 '''
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
+st.markdown('''Here we would like to add some controllers in order to ask the user to select the parameters of the ride
 
 1. Let's ask for:
 - date and time
@@ -25,16 +18,7 @@ Either as with the title by just creating a string (or an f-string). Or as with 
 - dropoff latitude
 - passenger count
 '''
-
-'''
-
-
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
+)
 # pass count
 pass_count = st.number_input('How many cars do you need?', step=1, min_value=1, max_value=40)
 st.write(pass_count)
@@ -83,17 +67,7 @@ if url == 'https://taxifare.lewagon.ai/predict':
 
     st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
 
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
-#2
+# 2. Let's build a dictionary containing the parameters for our API...
 params = {'pickup_datetime' : date_and_time,
           'pickup_longitude' : pi_long,
           'pickup_latitude' : pi_lat,
@@ -102,7 +76,9 @@ params = {'pickup_datetime' : date_and_time,
           'passenger_count': pass_count
           } #add other params expected by the API
 
-#3
+# 3. Let's call our API using the `requests` package...
+
 resposne = requests.get('https://taxifare.lewagon.ai/predict', params=params)
-#4
+# 4. Let's retrieve the prediction from the **JSON** returned by the API...
+# Finally, we can display the prediction to the user
 st.write(resposne.json())
